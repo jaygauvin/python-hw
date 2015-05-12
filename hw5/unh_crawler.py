d917f86938b269bedcf839e69f9784dacfce9b4e
jaygauvin <jaygauvin@gmail.com>
2015-05-12 09:36:42 -0400

    Enhanced Crawler

--- hw5/unh_crawler.py
+++ hw5/unh_crawler.py
@@ -5,7 +5,6 @@
 import urllib.request
 from urllib.error import  URLError
 import re
-import html
 import os
 import pickle
 
@@ -26,13 +25,16 @@ def visit_url(url, domain):
 		code=page.getcode()
 		if(code == 200):
 			content=page.read()
-			content_string = content.decode("utf-8")
-			regexp_title = re.compile('<title>(?P<title>(.*))</title>')
-			regexp_js_remove = re.compile('<script([^\'"]|"[^"]*"|\'[^\']*\')*?</script>')
-			regexp_junk_remove= re.compile("{[A-z0-9'\\;:,\s-]+}{1,2}|{}|(\\[A-z0-9]+\\[A-z0-9]+)/g") #remove stuff inside {}
-			regexp_body = re.compile('(<.*?>\\s*)+')
+			content_string = content.decode("ascii", "ignore")
 			
+			regexp_title = re.compile('<title>(?P<title>(.*))</title>')
+			regexp_body = re.compile('<body[^>]*>(?P<body>([\s\S]*))<\/body>')
 			regexp_url = re.compile("http://"+domain+"[/\w+]*")
+		
+			regexp_js_remove = re.compile('<script([^\'"]|"[^"]*"|\'[^\']*\')*?</script>') #remove javascript
+			regexp_junk_remove= re.compile("{[A-z0-9'\\;:,\s-]+}{1,2}|{}|(\\[A-z0-9]+\\[A-z0-9]+)/g") #remove stuff inside {}
+			regexp_text_remove = re.compile('(<.*?>\\s*)+|&[#A-z0-9]+;') #remove the tags < > and &amp; &nbsp; html encoding
+			regexp_space_fix = re.compile('\s{2,}')
 
 			result = regexp_title.search(content_string, re.IGNORECASE)
 
@@ -41,16 +43,18 @@ def visit_url(url, domain):
 				print(title)
 				print("===================================================")
 			
-			result = str(result)
-			result = regexp_js_remove.sub(" ", content_string) #remove javascript
-			result = regexp_body.sub(" ", result) #remove the tags < >
-			result = regexp_junk_remove.sub(" ", result) #remove some junk
-			result = result.replace("\'", "'").replace("\r\n", "").replace("{}","")
-			result = html.unescape(result).encode("utf-8") #conver the &amp; stuff to regular chars
-						
+			result = regexp_body.search(content_string, re.IGNORECASE) #remove binary problems, get text within body tags
+
 			if result:
-				url_data.append((url, result))
-				print(result)
+				result = result.group("body")
+				result = regexp_js_remove.sub(" ", result) #remove javascript
+				result = regexp_text_remove.sub(" ", result) #remove the tags < >
+				result = regexp_junk_remove.sub(" ", result) #remove some junk
+				result = regexp_space_fix.sub(" ", result) #remove extra spaces
+											
+				if result:
+					url_data.append((url, result))
+					print(result)
 
 			for (urls) in re.findall(regexp_url, content_string):
 					if(urls  not in crawler_backlog or crawler_backlog[urls] != 1):
